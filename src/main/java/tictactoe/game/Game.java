07abebbc64c4f7ef3711ac3a77f55ad190dcf1b2
Fanil Suratwala <Fanil-Suratwala>
2020-07-06 08:58:21 +0530

    styling

--- src/main/java/tictactoe/game/Game.java
+++ src/main/java/tictactoe/game/Game.java
@@ -8,14 +8,13 @@ import tictactoe.page.GamePage;
  * @author Fanil Suratwala
  * 
  */
-public class Game extends GamePage
-{
-   @Test
-   public void game() {
-	   launchGame();
-	   setXorO('x');		   	    // choose between 'x' or 'o'
-	   setDifficulty("Medium");	// Easy, Medium, Impossible
-	   setRounds(5);
-	   play();
-   }
+public class Game extends GamePage {
+	@Test
+	public void game() {
+		launchGame();
+		setXorO('x'); // choose between 'x' or 'o'
+		setDifficulty("Medium"); // Easy, Medium, Impossible
+		setRounds(5);
+		play();
+	}
 }
